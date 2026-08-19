dbb32dc78a76d38a4024c489bf4a73c37c7d04bc
AlexandredeOliveiraCoutinho11BTi <162030541+AlexandredeOliveiraCoutinho11BTi@users.noreply.github.com>
2026-08-19 11:45:24 -0300

    teste backend

--- index.js
+++ index.js
@@ -1,9 +1,145 @@
 import express from "express";
+import mysql2 from "mysql2";
+import cors from "cors";
 
 const app = express();
 
+app.use(express.json());
+app.use(cors());
+
 app.get("/", (req, res) => {
     res.send("Servidor funcionando!");
+});
+
+const sql = mysql2.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+});
+
+app.post("/add-movie", (req, res) => {
+    const {
+        title,
+        genre,
+        duration,
+        classificacao_etaria
+    } = req.body;
+
+    const insertCommand = `
+        INSERT INTO filmes_AlexandreCoutinhoLucasNunes
+        (title, genre, duration, classificacao_etaria)
+        VALUES (?, ?, ?, ?)
+    `;
+
+    sql.query(
+        insertCommand,
+        [title, genre, duration, classificacao_etaria],
+        (error) => {
+            if (error) {
+                console.log(error);
+
+                return res.status(500).json({
+                    error: "Erro ao adicionar filme"
+                });
+            }
+
+            res.status(201).json({
+                message: "Filme adicionado com sucesso"
+            });
+        }
+    );
+});
+
+app.delete("/delete-movie/:id", (req, res) => {
+    const { id } = req.params;
+
+    const deleteCommand = `
+        DELETE FROM filmes_AlexandreCoutinhoLucasNunes
+        WHERE id = ?
+    `;
+
+    sql.query(
+        deleteCommand,
+        [id],
+        (error) => {
+            if (error) {
+                console.log(error);
+
+                return res.status(500).json({
+                    error: "Erro ao deletar filme"
+                });
+            }
+
+            res.status(200).json({
+                message: "Filme deletado com sucesso"
+            });
+        }
+    );
+});
+
+app.get("/all-movies", (req, res) => {
+    const selectCommand = `
+        SELECT * FROM filmes_AlexandreCoutinhoLucasNunes
+    `;
+
+    sql.query(
+        selectCommand,
+        (error, data) => {
+            if (error) {
+                console.log(error);
+
+                return res.status(500).json({
+                    error: "Erro ao buscar filmes"
+                });
+            }
+
+            res.status(200).json(data);
+        }
+    );
+});
+
+app.put("/update-movie", (req, res) => {
+    const {
+        id,
+        title,
+        genre,
+        duration,
+        classificacao_etaria
+    } = req.body;
+
+    const updateCommand = `
+        UPDATE filmes_AlexandreCoutinhoLucasNunes
+        SET title = ?,
+            genre = ?,
+            duration = ?,
+            classificacao_etaria = ?
+        WHERE id = ?
+    `;
+
+    sql.query(
+        updateCommand,
+        [
+            title,
+            genre,
+            duration,
+            classificacao_etaria,
+            id
+        ],
+        (error) => {
+            if (error) {
+                console.log(error);
+
+                return res.status(500).json({
+                    message: "Erro ao alterar filme"
+                });
+            }
+
+            res.status(200).json({
+                message: "Filme alterado com sucesso!"
+            });
+        }
+    );
 });
 
 export default app;
