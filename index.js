c64df12740d224896cab6fc8a6d2be628e2b595f
AlexandredeOliveiraCoutinho11BTi <162030541+AlexandredeOliveiraCoutinho11BTi@users.noreply.github.com>
2026-08-19 11:47:37 -0300

    agora vai

--- index.js
+++ index.js
@@ -12,12 +12,12 @@ app.get("/", (req, res) => {
 });
 
 const sql = mysql2.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
-});
+     host: "benserverplex.ddns.net",
+      user: "alunos",
+       password: "senhaAlunos",
+        database: "alunos_filmes03MB" });
 
+        
 app.post("/add-movie", (req, res) => {
     const {
         title,
